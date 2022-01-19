a9b9606d2e7a8cc7197689128b93f8efbc9a67e2
Yehor Kostenko <yehor.kostenko@bini.games>
2022-01-19 17:19:20 +0200

    PlayerRespawn added, PlayerMove fixes

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,7 @@ public class PlayerMove : MonoBehaviour
     public float speed = 3;
     public Rigidbody2D _rigidbody;
 
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -19,17 +20,17 @@ public class PlayerMove : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
-
+        
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
             _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
 
-        Vector2 characherScale = transform.localScale;
+        Vector2 characterScale = transform.localScale;
 
         if (horizontalInput >= 0)
-            characherScale.x = 4;
-        else if (horizontalInput <= 0) characherScale.x = -4;
+            characterScale.x = 3.708483f;
+        else if (horizontalInput <= 0) characterScale.x = -3.708483f;
 
-        transform.localScale = characherScale;
+        transform.localScale = characterScale;
     }
 
     
---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform spawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+            col.transform.position = spawnPoint.position;
+    }
+}
