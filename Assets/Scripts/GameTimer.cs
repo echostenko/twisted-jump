4aba2090a6f3d20acda1763ef6c75f3ec6e5d04e
Yehor Kostenko <yehor.kostenko@bini.games>
2022-01-20 23:31:17 +0200

    GameTimer added

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    private float currentTime = 0f;
+    private float startTime = 10f;
+    [SerializeField]
+    public Text gameTimer;
+    [SerializeField] 
+    public GameObject gameOver;
+    public Transform parent;
+    public GameObject player;
+
+    
+
+    private void Start()
+    {
+        currentTime = startTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        gameTimer.text = currentTime.ToString("0");
+        if (currentTime <= 0)
+        {
+            DestroyImmediate(gameObject);
+            Destroy(player);
+            Instantiate(gameOver, new Vector3(0, 0, 0), gameOver.transform.rotation, parent);
+        }
+        
+    }
+}
