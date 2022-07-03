256546daa75e7256b7cd00de70a8a470e64bd07a
Yehor Kostenko <yehor.kostenko@bini.games>
2022-07-03 13:34:33 +0300

    PlatformFactory and PlatformPool added

---
+++ Assets/Scripts/Platforms/PlatformFactory.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace Platforms
+{
+    public class PlatformFactory : IObjectFactory
+    {
+        private readonly IAssetProvider assetProvider;
+        private const string platformPath = "Prefabs/Platforms/Wood Platform";
+
+        public PlatformFactory(IAssetProvider assetProvider) =>
+            this.assetProvider = assetProvider;
+
+        public GameObject CreateObject(Vector3 position, Transform parent = null) =>
+            Object.Instantiate(assetProvider.Load<GameObject>(platformPath), position, Quaternion.identity, parent);
+    }
+}
---
+++ Assets/Scripts/Platforms/PlatformPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Data;
+using Interfaces;
+using UnityEngine;
+
+namespace Platforms
+{
+    public class PlatformPool : IObjectPool
+    {
+        private readonly List<GameObject> availableItems = new List<GameObject>();
+        private readonly List<GameObject> takenItems = new List<GameObject>();
+        private readonly IObjectFactory platformFactory;
+        private readonly GameSettings gameSettings;
+
+        public PlatformPool(IObjectFactory platformFactory, GameSettings gameSettings)
+        {
+            this.platformFactory = platformFactory;
+            this.gameSettings = gameSettings;
+        }
+
+        public void Initialize()
+        {
+            for (var i = 0; i < gameSettings.ItemsCount; i++)
+                availableItems.Add(platformFactory.CreateObject(new Vector3(-5000, 5000, 0)));
+        }
+
+        public GameObject GetItemFromPool()
+        {
+            var platform = availableItems[0];
+            availableItems.Remove(platform);
+            takenItems.Add(platform);
+            platform.SetActive(true);
+
+            return platform;
+        }
+
+        public void SetItemToPool(GameObject item)
+        {
+            if (!takenItems.Contains(item))
+                return;
+
+            availableItems.Add(item);
+            takenItems.Remove(item);
+            item.SetActive(false);
+        }
+    }
+}
