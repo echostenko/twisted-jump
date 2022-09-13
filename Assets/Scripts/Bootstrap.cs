09e80a39d5f1082bc7f0667e3ec45c4df8d7c894
Yehor Kostenko <yehor.kostenko@bini.games>
2022-09-13 11:06:11 +0300

    Inject serviceLocator added to Bootstrap

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,24 +1,15 @@
-﻿using Data;
-using Services;
+﻿using Services;
 using UnityEngine;
 using Zenject;
 
 public class Bootstrap : MonoBehaviour
 {
-    [SerializeField] private Transform firstSpawner;
-    [SerializeField] private Transform secondSpawner;
-    [SerializeField] private Transform thirdSpawner;
-    [SerializeField] private Transform platforms;
-    [SerializeField] private GameSettings gameSettings;
     private ServiceLocator serviceLocator;
 
     [Inject]
     public void Construct(ServiceLocator serviceLocator) => 
         this.serviceLocator = serviceLocator;
 
-    private void Awake()
-    {
-        serviceLocator = new ServiceLocator(gameSettings, firstSpawner, secondSpawner, thirdSpawner, platforms);
+    private void Awake() => 
         serviceLocator.Initialize();
-    }
 }
