816e04cf3d12cb3c6bd2c8e1077dfc4982e4f9f4
Yehor Kostenko <yehor.kostenko@bini.games>
2022-09-13 10:02:06 +0300

    Bootstrap added

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+using Services;
+using UnityEngine;
+using Zenject;
+
+public class Bootstrap : MonoBehaviour
+{
+    [SerializeField] private Transform firstSpawner;
+    [SerializeField] private Transform secondSpawner;
+    [SerializeField] private Transform thirdSpawner;
+    [SerializeField] private Transform platforms;
+    [SerializeField] private GameSettings gameSettings;
+    private ServiceLocator serviceLocator;
+
+    [Inject]
+    public void Construct(ServiceLocator serviceLocator) => 
+        this.serviceLocator = serviceLocator;
+
+    private void Awake()
+    {
+        serviceLocator = new ServiceLocator(gameSettings, firstSpawner, secondSpawner, thirdSpawner, platforms);
+        serviceLocator.Initialize();
+    }
+}
